9f78b5b7c8d57c6cda0ef67ffda39719bf8cc159
skyleira <tyler.b.hively@gmail.com>
2024-08-31 21:00:03 -0400

    Added code to accept the display of the first name.

--- Checkpoint2.14.cpp
+++ Checkpoint2.14.cpp
@@ -8,17 +8,22 @@
 */
 
 #include <iostream>
+#include <string>
 
 int main()
 {
     char firstinitial;
     char middleinitial;
     char lastinitial;
+    std::string firstName;
     std::cout << "Please enter the initial of your first name: ";
     std::cin >> firstinitial;
     std::cout << "Please enter the initial of your middle name: ";
     std::cin >> middleinitial;
     std::cout << "Please enter the initial of your last name: ";
     std::cin >> lastinitial;
-    std::cout << "Your initials are: " << firstinitial << middleinitial << lastinitial;
+    std::cout << "Your initials are: " << firstinitial << middleinitial << lastinitial << std::endl;
+    std::cout << "Please enter your first name: ";
+    std::cin >> firstName;
+    std::cout << "Your first name is: " << firstName;
 }
